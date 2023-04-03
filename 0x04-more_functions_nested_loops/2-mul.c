df58d6ae6a0c41467804c97522e6d5e4c4ebbbf3
Tumelo <letsoalotums@gamil.com>
2023-04-03 14:08:34 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * nul - Multiplies two intergers
+ * mul - Multiplies two intergers
  * @x: The first number is multiply
  * @y: the second number to multiply with the first number
- * Retur: Theresult of the multiplication between x paran and y paran
+ * Return: Theresult of the multiplication between x paran and y paran
  */
 
 int mul(int x, int  y)
 
 {
-
 	return (x * y);
 }
